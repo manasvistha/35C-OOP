b90a217eeb43f448a647c9c428421b756cc430b5
manasvistha <158544680+manasvistha@users.noreply.github.com>
2024-04-12 12:36:21 +0545

    week 1 Upload

--- week 1/hello.java
+++ week 1/hello.java
@@ -6,5 +6,14 @@ public class hello{
         /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("hello world");
 
+/*2. Extend/Modify the above program to print two additional lines:
+This is my first program 
+I am on module ST4003CEM
+System.out.print (not println) and produces the same output.*/
+        System.out.println("This is my first program");
+        System.out.println("I am on module ST4003CEM");
+
+  
     }
-}
+}    
+
