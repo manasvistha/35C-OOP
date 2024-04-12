458bdf5c983fb6463bfe915d68ccd870b9d6c114
manasvistha <158544680+manasvistha@users.noreply.github.com>
2024-04-12 12:28:21 +0545

    week 1 task 1 question

--- week 1/hello.java
+++ week 1/hello.java
@@ -3,6 +3,7 @@
 public class hello{
 
     public static void main(String[] args) {
+        /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("hello world");
 
     }
